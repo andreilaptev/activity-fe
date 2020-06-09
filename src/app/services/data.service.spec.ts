62107458970e56646b9cb57ea182a42acf57248e
Andrei <andrey.laptev@outlook.com>
2020-06-09 00:31:17 -0400

    data service test added

--- src/app/services/data.service.spec.ts
+++ src/app/services/data.service.spec.ts
@@ -24,4 +24,27 @@ fdescribe('DataService', () => {
   it('DataService should be created', inject([DataService], (dataService: DataService) => {
     expect(dataService).toBeTruthy();
   }));
+
+  it('should return an Observables of Activities', () => {
+    const testActivities = [
+      {
+        id: 100,
+        name: 'activity1'
+      },
+      {
+        id: 200,
+        name: 'activity2'
+      }
+    ];
+
+    dataService.getActivitiesList().subscribe(
+      activities => {
+        expect(activities[0].length(2));
+        expect(activities).toEqual(testActivities);
+      });
+
+      const request = httpMock.expectOne(`${dataService.url}activities`);
+      expect(request.request.method).toBe("GET");
+      request.flush(testActivities);
+  })
 });
